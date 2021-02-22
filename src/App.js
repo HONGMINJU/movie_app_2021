e20c8520a2c697aec48f9d1dd3adb923fb63fafe
HONGMINJU <tlsrlgkrry@naver.com>
2021-02-22 20:57:48 +0900

    final

--- src/App.js
+++ src/App.js
@@ -1,50 +1,66 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movies";
-
-class App extends React.Component{
+import "./App.css";
+// App은 state가 필요하기 때문에 class로 선언
+class App extends React.Component {
   // component의 life cycle 
   // mount(생성: constructor -> render -> componentDidMount)
   // update(업데이트: update -> render -> componentDidUpdated)
   // unmount(삭제: componentWillUnmount)
 
-  state={
+  state = {
     isLoading: true,
-    movies:[]
+    movies: []
   }
   // render함수 호출 후 자동적으로 실행되는 함수
   // 6초 후에 isLoading값을 바꾸도록 설정함
 
 
   // axios가 끝날 때 까지 기다려줌 => async, await
-  getMovies = async () => { 
+  getMovies = async () => {
     // const movies=await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
     // console.log(movies.data.data.movies);
-    const {data : { data :{movies}}}=await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoading:false }); //{movies:movies}=={movies}
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false }); //{movies:movies}=={movies}
   }
 
 
   // render함수 호출 후 자동적으로 실행되는 함수(componentDidMount)
   // 6초 후에 isLoading값을 바꾸도록 설정함
-  componentDidMount(){ 
+  componentDidMount() {
     // setTimeout(()=>{
     //   this.setState({isLoading:false}); //여기서 다시 render호출
     // }, 6000);
     this.getMovies();
   }
-  render(){
-    const {isLoading , movies}=this.state;
-    return <div>{isLoading?"Loading . . .":movies.map(movie=>{
-      console.log(movie);
-      return <Movie 
-      key={movie.id}
-      id={movie.id} 
-      year={movie.year} 
-      title={movie.title} 
-      summary={movie.summary} 
-      poster={movie.medium_cover_image} />
-    })}</div>;
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading . . .</span>
+          </div>
+        ) : (
+          <div className="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+
+            </div>
+          )
+        }
+
+      </section>);
   }
 }
 
--- src/Movies.js
+++ src/Movies.js
@@ -1,19 +1,34 @@
-import  React  from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-//movie component는 state가 필요없음 
+// movie component는 state가 필요없음 
 // => class로 선언하지않고 function으로 선언해도 됨
 
-function Movie({id,year,title,summary,poster}){
-    return <h5>{title}</h5>;
+function Movie({ id, year, title, summary, poster, genres }) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title}></img>
+            <div className="movie_data">
+                <h3 className="movie_title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="movie_genres">
+                    {genres.map((genre, index) => (
+                    <li key = {index} className="genres_genre">{genre}</li>
+                    ))}
+                </ul>
+                <p className="movie_summary">{summary.slice(0,180)}...</p>
+            </div>
+        </div>);
 }
 
-Movie.propTypes ={
-    id:PropTypes.number.isRequired,
-    year:PropTypes.number.isRequired,
-    title:PropTypes.string.isRequired,
-    summary:PropTypes.string.isRequired,
-    poster:PropTypes.string.isRequired
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
